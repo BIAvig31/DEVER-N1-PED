238f2773af2a7899eb517a47f4f41a0b7412f34e
Beatriz Vignoles <141172861+BIAvig31@users.noreply.github.com>
2023-10-24 14:34:51 -0300

    Add files via upload

---
+++ exemplo2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 13:52:20 2023
+
+@author: 202301488648
+"""
+
+def soma (x, y):
+    return x + y
+print(soma(3, 5))
+print(soma(1237, 2356))
+
+def simples(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
